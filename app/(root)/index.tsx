84b3d963d5d450374d170dcf20ccb328b095e1f0
Petrukhin <dartmol2300@gmail.com>
2024-11-28 00:04:38 +0300

    fix: after merge cleanup

--- app/(root)/index.tsx
+++ app/(root)/index.tsx
@@ -1,4 +1,3 @@
-
 import React, { useContext, useEffect, useState } from "react";
 import { Platform } from "react-native";
 import {
@@ -16,11 +15,10 @@ import Carousel from "react-native-reanimated-carousel";
 import { ICarouselInstance } from "react-native-reanimated-carousel";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Animated, { interpolate } from "react-native-reanimated";
-import { AudioRepository, FeedResponse } from "@/repositories/AudioRepository";
+import { AudioRepository } from "@/repositories/AudioRepository";
 import { useAudioPlayer } from "expo-audio";
 import { Middleware } from "@/repositories/Middleware";
 import { router } from "expo-router";
-import { Result } from "@/repositories/Response";
 import { useSession } from "@/context/AuthContext";
 
 const sh = Dimensions.get("window").height * 0.5;
@@ -30,42 +28,42 @@ const h = Dimensions.get("window").height;
 
 function Item(it: number, {id}: {id: string}, isPaused: boolean, setIsPaused: (isPaused: boolean) => void): React.JSX.Element {
   return (
-    <Animated.View
-      style={{
-        display: "flex",
-        flex: 1,
-        alignSelf: "center",
-        marginBottom: 0.05 * h,
-        marginTop: 0.2 * h,
-      }}
-    >
-      <SquircleView
-        squircleParams={{
-          cornerRadius: 60,
-          cornerSmoothing: 1,
-          fillColor: "#a23ba5",
-        }}
-        style={{
-          width: aspectRatio * sh,
-          height: sh,
-          alignSelf: "center",
-          margin: 0,
-        }}
+      <Animated.View
+          style={{
+            display: "flex",
+            flex: 1,
+            alignSelf: "center",
+            marginBottom: 0.05 * h,
+            marginTop: 0.2 * h,
+          }}
       >
-        <View>
-          <TrackForm form={{ id: id }} setForm={() => {}}></TrackForm>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              if (isPaused) setIsPaused(false);
-              else setIsPaused(true);
+        <SquircleView
+            squircleParams={{
+              cornerRadius: 60,
+              cornerSmoothing: 1,
+              fillColor: "#a23ba5",
             }}
-          >
-            <Text>{it.toString()}</Text>
-          </TouchableOpacity>
-        </View>
-      </SquircleView>
-    </Animated.View>
+            style={{
+              width: aspectRatio * sh,
+              height: sh,
+              alignSelf: "center",
+              margin: 0,
+            }}
+        >
+          <View>
+            <TrackForm form={{ id: id }} setForm={() => {}}></TrackForm>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                  if (isPaused) setIsPaused(false);
+                  else setIsPaused(true);
+                }}
+            >
+              <Text>{it.toString()}</Text>
+            </TouchableOpacity>
+          </View>
+        </SquircleView>
+      </Animated.View>
   );
 }
 
@@ -103,20 +101,20 @@ function Index(): React.JSX.Element {
 
     const preload = async () => {
       const card1 = await Middleware.withRefreshToken(
-        {
-          accessToken: accessToken,
-          refresh: refresh,
-          refreshToken: refreshToken,
-        },
-        AudioRepository.feed,
+          {
+            accessToken: accessToken,
+            refresh: refresh,
+            refreshToken: refreshToken,
+          },
+          AudioRepository.feed,
       );
       const card2 = await Middleware.withRefreshToken(
-        {
-          accessToken: accessToken,
-          refresh: refresh,
-          refreshToken: refreshToken,
-        },
-        AudioRepository.feed,
+          {
+            accessToken: accessToken,
+            refresh: refresh,
+            refreshToken: refreshToken,
+          },
+          AudioRepository.feed,
       );
 
       if (card1.success && card2.success) {
@@ -154,68 +152,6 @@ function Index(): React.JSX.Element {
     else player?.play();
   }, [isPaused])
 
-  const [[isAccessTokenLoading, accessToken], setAccessToken] = useStorageState("accessToken");
-  const [[isRefreshTokenLoading, refreshToken], setRefreshToken] = useStorageState("refreshToken");
-
-  const [trackID, setTrackID] = React.useState<string | null>(null);
-  const [curIndex, setCurIndex] = React.useState<number | null>(null);
-
-  useEffect(() => {
-    if (!accessToken || !refreshToken) return;
-
-    const preload = async () => {
-      const card1 = await Middleware.withRefreshToken(
-        {
-          accessToken: accessToken,
-          setAccessToken: setAccessToken,
-          refreshToken: refreshToken,
-          setRefreshToken: setRefreshToken,
-        },
-        AudioRepository.feed,
-      );
-      const card2 = await Middleware.withRefreshToken(
-        {
-          accessToken: accessToken,
-          setAccessToken: setAccessToken,
-          refreshToken: refreshToken,
-          setRefreshToken: setRefreshToken,
-        },
-        AudioRepository.feed,
-      );
-
-      if (card1.success && card2.success) {
-        setTrackID(card1.data.id);
-        setIsPaused(false);
-        setCurIndex(0);
-        setIt([{index: 0, id: card1.data.id, name: card1.data.name, artist: card1.data.beatmaker.pseudonym}, {index: 1, id: card2.data.id, name: card2.data.name, artist: card2.data.beatmaker.pseudonym}]);
-        return;
-      }
-
-      setCurIndex(null);
-      router.push("/(auth)/login");
-    };
-
-    preload().catch((e) => console.error(e));
-  }, [isAccessTokenLoading, isRefreshTokenLoading]);
-
-  useEffect(() => {
-    const play = async () => {
-      if (!trackID) return;
-      player?.pause();
-      player?.replace({uri: `http://localhost:8083/v1/audio/${trackID}/stream`});
-      player?.play();
-      console.log("playing");
-    };
-
-    play().catch((e) => console.error(e));
-  }, [curIndex, isAccessTokenLoading, isRefreshTokenLoading]);
-
-  const [isPaused, setIsPaused] = React.useState(true);
-  useEffect(() => {
-    if (isPaused) player?.pause();
-    else player?.play();
-  }, [isPaused])
-
   const onSnapToItem = async (index: number) => {
     if (!accessToken || !refreshToken) {
       return;
@@ -226,12 +162,12 @@ function Index(): React.JSX.Element {
     setIsPaused(false);
     if (index === it.length - 1) {
       const data = await Middleware.withRefreshToken(
-        {
-          accessToken: accessToken,
-          refresh: refresh,
-          refreshToken: refreshToken,
-        },
-        AudioRepository.feed,
+          {
+            accessToken: accessToken,
+            refresh: refresh,
+            refreshToken: refreshToken,
+          },
+          AudioRepository.feed,
       );
       if (data.success)
         setIt((prev) => [...prev, {index: it.length, id: data.data.id, name: data.data.name, artist: data.data.beatmaker.pseudonym}]);
@@ -244,47 +180,47 @@ function Index(): React.JSX.Element {
   };
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaView
-        style={{
-          display: "flex",
-          flex: 1,
-          height: "100%",
-          flexDirection: "column",
-        }}
-        id={"carousel-component"}
-      >
-        <Carousel
-          ref={ref}
-          customAnimation={animationStyle}
-          scrollAnimationDuration={500}
-          width={w}
-          height={h}
-          data={it}
-          style={{
-            height: h,
-            alignSelf: "center",
-            alignItems: "center",
-            justifyContent: "center",
-            width: w,
-          }}
-          windowSize={2}
-          snapEnabled={true}
-          pagingEnabled={true}
-          mode="parallax"
-          overscrollEnabled={false}
-          loop={false}
-          modeConfig={{
-            parallaxScrollingOffset: 50,
-          }}
-          vertical={true}
-          onSnapToItem={onSnapToItem}
-          renderItem={({ item }) => {
-            return Item(item.index, {id: item.id}, isPaused, setIsPaused);
-          }}
-        />
-      </SafeAreaView>
-    </GestureHandlerRootView>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <SafeAreaView
+            style={{
+              display: "flex",
+              flex: 1,
+              height: "100%",
+              flexDirection: "column",
+            }}
+            id={"carousel-component"}
+        >
+          <Carousel
+              ref={ref}
+              customAnimation={animationStyle}
+              scrollAnimationDuration={500}
+              width={w}
+              height={h}
+              data={it}
+              style={{
+                height: h,
+                alignSelf: "center",
+                alignItems: "center",
+                justifyContent: "center",
+                width: w,
+              }}
+              windowSize={2}
+              snapEnabled={true}
+              pagingEnabled={true}
+              mode="parallax"
+              overscrollEnabled={true}
+              loop={false}
+              modeConfig={{
+                parallaxScrollingOffset: 50,
+              }}
+              vertical={true}
+              onSnapToItem={onSnapToItem}
+              renderItem={({ item }) => {
+                return Item(item.index, {id: item.id}, isPaused, setIsPaused);
+              }}
+          />
+        </SafeAreaView>
+      </GestureHandlerRootView>
   );
 }
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,42 +1,43 @@
 import * as SplashScreen from "expo-splash-screen";
 import "react-native-reanimated";
-import { Slot } from "expo-router";
+import {Slot} from "expo-router";
 import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
+    DarkTheme,
+    DefaultTheme,
+    ThemeProvider,
 } from "@react-navigation/native";
-import { useColorScheme } from "@/hooks/useColorScheme.ts";
-import { useFonts } from "expo-font";
-import { useEffect } from "react";
-import { SessionProvider } from "@/context/AuthContext";
-import { UserProvider } from "@/context/UserContext";
+import {useColorScheme} from "@/hooks/useColorScheme.ts";
+import {useFonts} from "expo-font";
+import {useEffect} from "react";
+import {SessionProvider} from "@/context/AuthContext";
+import {UserProvider} from "@/context/UserContext";
+import {enableExperimentalWebImplementation} from "react-native-gesture-handler";
 
 enableExperimentalWebImplementation(true);
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require("@/assets/fonts/SpaceMono-Regular.ttf"),
-  });
+    const colorScheme = useColorScheme();
+    const [loaded] = useFonts({
+        SpaceMono: require("@/assets/fonts/SpaceMono-Regular.ttf"),
+    });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
     }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-  return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <UserProvider>
-        <SessionProvider>
-          <Slot />
-        </SessionProvider>
-      </UserProvider>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+            <UserProvider>
+                <SessionProvider>
+                    <Slot/>
+                </SessionProvider>
+            </UserProvider>
+        </ThemeProvider>
+    );
 }
